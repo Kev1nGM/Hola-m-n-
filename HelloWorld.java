4ce6881534ad32ccf530845225e12b113c64f4bf
Kev1nGM <kgm00@iesemilidarder.com>
2024-02-22 00:44:59 +0100

    Primer commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package practica_conflictes_git;
+import java.util.Scanner;
+/**  
+ * 
+ * 
+ * @author Kevin
+ */
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
